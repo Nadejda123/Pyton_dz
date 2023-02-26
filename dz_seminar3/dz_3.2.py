e460d705917f8b9fde8599ef0af052cc0944d955
«Nadejda <Nadejdaburca2@gmail.com>
2023-02-26 02:17:43 +0200

    Dz_ k 3 seminaru

--- dz_seminar3/dz_3.2.py
+++ dz_seminar3/dz_3.2.py
@@ -9,24 +9,8 @@
 #     6
 #     -> 5
 
-# a = [int(input())for _ in range (int(input('Ввежите размер массива: ')))]
-# x=int(input('Введите искомое число: '))
-# ind=0
-# for i in range(len(a)-1):
-#     if x-a[i]>x-a[i+1]:
-#         ind=i
-#     el
-#     print(a[ind])
 
 a = [int(input())for _ in range (int(input('Ввежите размер массива: ')))]
 x=int(input('Заданное число:'))
 b=[abs(a[i]-x) for i in range(len(a))]
 print(a[b.index(min(b))])
-
-# a=[int(input()) for _ in range(int(input()))]
-# b=int(input())
-# number=0
-# for i in range(len(a)):
-#     if (b-a[i])<b-number and b-a[i]>0:
-#         number=i
-# print(a[number])
